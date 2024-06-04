e8a0532cd93968c747d2d8e99c1292a6bc15c1bc
ls <ls.yano@unesp.br>
2024-06-03 21:13:34 -0300

    Allow editing in photo selection (profileSetup Screen)

--- src/Screens/ProfileSetup.js
+++ src/Screens/ProfileSetup.js
@@ -21,6 +21,7 @@ export default ({ route, navigation }) => {
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
     });
